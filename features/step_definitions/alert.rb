7bed8637b8c428971311a1a1f8332dd560aa466a
Cao Hoai Viet <hoaiviet@siliconstraits.com>
2016-04-14 17:24:32 +0700

    Wait to see the alert/ Get alert message

--- features/step_definitions/alert.rb
+++ features/step_definitions/alert.rb
@@ -1,7 +1,29 @@
+# For more information, please contact me
+# Cao Hoai Viet, email: viet.ch2612@gmail.com
+# or Appium ruby lib documents https://github.com/appium/ruby_lib/blob/master/docs/ios_docs.md
+
+# Accept the alert
 And(/^I accept the alert$/) do
   alert_accept
 end
 
-And(/^I dissmiss the alert$/) do
+# Dismiss the alert
+And(/^I dismiss the alert$/) do
   alert_dismiss
 end
+
+# See the alert
+And(/^I should see the alert$/) do
+  find_element :class_name, 'UIAAlert'
+end
+
+# Wait to see the alert
+And(/^I should see the alert$/) do
+  wait { find_element :class_name, 'UIAAlert' }
+end
+
+# Wait to see alert with text
+And(/^I wait to see the alert with text "([^\"]*)"$/) do |message|
+  wait { text '#{message}' }
+end
+
