c08c90e1075042ee1445a8365d1dc7bdcfd1e55b
Cao Hoai Viet <hoaiviet@siliconstraits.com>
2016-04-15 11:58:59 +0700

    Navigator steps

---
+++ features/step_definitions/navigate.rb
@@ -0,0 +1,10 @@
+# For more information, please contact me
+# Cao Hoai Viet, email: viet.ch2612@gmail.com
+# or Appium ruby lib documents https://github.com/appium/ruby_lib/blob/master/docs/ios_docs.md
+
+# Not tested
+# Go back
+Then(/^I go back$/) do
+  back
+end
+
