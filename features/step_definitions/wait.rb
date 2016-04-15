3d35d776de44783d61ea25588908f69e9daf9a38
Cao Hoai Viet <hoaiviet@siliconstraits.com>
2016-04-15 11:59:23 +0700

    Waiting steps

---
+++ features/step_definitions/wait.rb
@@ -0,0 +1,8 @@
+# For more information, please contact me
+# Cao Hoai Viet, email: viet.ch2612@gmail.com
+# or Appium ruby lib documents https://github.com/appium/ruby_lib/blob/master/docs/ios_docs.md
+
+# Default wait
+And(/^I wait$/) do 
+  #do something
+end
