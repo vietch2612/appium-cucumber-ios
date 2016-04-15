13d7d3df9f40a48bbcfd05dfdd96dfdc4e44054f
Cao Hoai Viet <hoaiviet@siliconstraits.com>
2016-04-15 11:58:38 +0700

    Button steps

---
+++ features/step_definitions/button.rb
@@ -0,0 +1,17 @@
+# For more information, please contact me
+# Cao Hoai Viet, email: viet.ch2612@gmail.com
+# or Appium ruby lib documents https://github.com/appium/ruby_lib/blob/master/docs/ios_docs.md
+
+Then(/^I go back$/) do
+  back
+end
+
+# Find a button by index
+And(/^I press the button number (\d+)$/) do |button_index|
+  button(button_index.to_i).click
+end
+
+# Find a button by text
+And(/^I press the "([^"]*)" button$/) do |button_text|
+  button(button_text).click
+end
