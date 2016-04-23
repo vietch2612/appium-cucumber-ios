00e76588f179d37ed946f434015f55b00e3d5cb7
Cao Hoai Viet <hoaiviet@siliconstraits.com>
2016-04-23 11:09:48 +0700

    demo

--- features/sign_up.feature
+++ features/sign_up.feature
@@ -2,4 +2,6 @@ Feature: Sign up
 
   Scenario: Sign up with invalid email format
     When I accept Location Service permission
-    Then I press a button labelled "Try as guest"
+    And I wait
+    And I wait for 2 seconds
+    And I wait for button "Try as guest" appear
--- features/step_definitions/wait.rb
+++ features/step_definitions/wait.rb
@@ -2,7 +2,17 @@
 # Cao Hoai Viet, email: viet.ch2612@gmail.com
 # or Appium ruby lib documents https://github.com/appium/ruby_lib/blob/master/docs/ios_docs.md
 
-# Default wait
-And(/^I wait$/) do 
-  #do something
+# Default wait, 30 seconds
+And(/^I wait$/) do
+  default_wait
+end
+
+# Wait for extract seconds
+And(/^I wait for (\d+) seconds$/) do |time|
+  set_wait time.to_i
+end
+
+# Wait for button visible
+And(/^I wait for button "([^"]*)" appear$/) do |button_text|
+  wait { text button_text}
 end
